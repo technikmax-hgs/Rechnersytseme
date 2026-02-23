83ef2d8ffd2fdd3e955c4bc310ac9f3e5680f330
technikmax-hgs <max-roensch@gmx.net>
2026-02-23 10:19:40 +0100

    Update Webseit Temperaturkruve

---
+++ www/graph.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+filename="/home/gruppe2/temperatur1.rrd"
+rrdtool graph - \
+             --imgformat 'PNG' \
+             --width 640 \
+             --height 300\
+             --start -1hour \
+             --end now \
+             --vertical-label "Grad Celsius" \
+             --alt-autoscale \
+             --title Temperatur \
+             DEF:temp1=$filename:temp1:AVERAGE \
+             AREA:temp1#CCFFCC: \
+             LINE1:temp1#0000FF:'Temperatur DS18B20' \
+             GPRINT:temp1:MIN:"Min\\:  %3.2lf C  "  \
+             GPRINT:temp1:MAX:"Max\\: %3.2lf C  " \
+             GPRINT:temp1:AVERAGE:"Avg\\: %3.2lf C " \
+             GPRINT:temp1:LAST:"Aktuell\\: %3.2lf C "
